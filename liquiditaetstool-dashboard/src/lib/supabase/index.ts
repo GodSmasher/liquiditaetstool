311e0033444f5c0974ceed4e94d586b198d6a9d8
Sarah Vogel <vogels671@gmail.com>
2025-11-10 14:08:38 +0100

    Deploy 5.0

--- liquiditaetstool-dashboard/src/lib/supabase/index.ts
+++ liquiditaetstool-dashboard/src/lib/supabase/index.ts
@@ -1,6 +1,5 @@
 // Re-export all Supabase utilities
 export { createClient } from './client'
 export { createClient as createServerClient } from './server'
-export { updateSession } from './middleware'
 export type { Database } from './types'
 
